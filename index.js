5e98d1cb185069a8d63e11ee460bc76a4e238a89
MAJID <mohammadmajid55@gmail.com>
2019-04-05 16:36:03 +0000

    changes bold

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ var chalk = require("chalk");
 
 var message = "Hello " + chalk.yellow("World");
 console.log(message);
+
+var big = require("chalk");
+var saying = chalk.bold("I AM VERY BIG TEXT");
+console.log(saying);
